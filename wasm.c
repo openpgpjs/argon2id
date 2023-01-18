3fa3654ea6122b27e5bb963483c9558b79e969a2
larabr <7375870+larabr@users.noreply.github.com>
2023-01-18 20:57:16 +0100

    Add SIMD SSSE3 optimisations
    Compile with flags: `-msimd128 -mssse3`
    
    Unsure about the speedup (if any) over SSE2

--- wasm.c
+++ wasm.c
@@ -8,11 +8,31 @@
 #undef EMSCRIPTEN_KEEPALIVE
 #define EMSCRIPTEN_KEEPALIVE __attribute__((used)) __attribute__((retain))
 
-#if defined(__SSE2__)
+#if defined(__SSSE3__) || defined(__SSE2__)
 #include <emmintrin.h>
 
-#define _mm_roti_epi64(r, c)                                                   \
-    _mm_xor_si128(_mm_srli_epi64((r), -(c)), _mm_slli_epi64((r), 64 - (-(c))))
+#if defined(__SSSE3__)
+#include <tmmintrin.h>
+  #define r16                                                                    \
+      (_mm_setr_epi8(2, 3, 4, 5, 6, 7, 0, 1, 10, 11, 12, 13, 14, 15, 8, 9))
+  #define r24                                                                    \
+      (_mm_setr_epi8(3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10))
+  #define _mm_roti_epi64(x, c)                                                   \
+      (-(c) == 32)                                                               \
+          ? _mm_shuffle_epi32((x), _MM_SHUFFLE(2, 3, 0, 1))                      \
+          : (-(c) == 24)                                                         \
+                ? _mm_shuffle_epi8((x), r24)                                     \
+                : (-(c) == 16)                                                   \
+                      ? _mm_shuffle_epi8((x), r16)                               \
+                      : (-(c) == 63)                                             \
+                            ? _mm_xor_si128(_mm_srli_epi64((x), -(c)),           \
+                                            _mm_add_epi64((x), (x)))             \
+                            : _mm_xor_si128(_mm_srli_epi64((x), -(c)),           \
+                                            _mm_slli_epi64((x), 64 - (-(c))))
+#else /* SSE2 */
+  #define _mm_roti_epi64(r, c)                                                   \
+      _mm_xor_si128(_mm_srli_epi64((r), -(c)), _mm_slli_epi64((r), 64 - (-(c))))
+#endif
 static __m128i fBlaMka(__m128i x, __m128i y) {
     const __m128i z = _mm_mul_epu32(x, y);
     return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
@@ -59,33 +79,69 @@ static __m128i fBlaMka(__m128i x, __m128i y) {
     B0 = _mm_roti_epi64(B0, -63);                                          \
     B1 = _mm_roti_epi64(B1, -63);                                          \
   } while ((void)0, 0)
-#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                        \
-  do {                                                                     \
-    __m128i t0 = D0;                                                       \
-    __m128i t1 = B0;                                                       \
-    D0 = C0;                                                               \
-    C0 = C1;                                                               \
-    C1 = D0;                                                               \
-    D0 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t0, t0));               \
-    D1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(D1, D1));               \
-    B0 = _mm_unpackhi_epi64(B0, _mm_unpacklo_epi64(B1, B1));               \
-    B1 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(t1, t1));               \
-  } while ((void)0, 0)
 
-#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                      \
-  do {                                                                     \
-    __m128i t0, t1;                                                        \
-    t0 = C0;                                                               \
-    C0 = C1;                                                               \
-    C1 = t0;                                                               \
-    t0 = B0;                                                               \
-    t1 = D0;                                                               \
-    B0 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(B0, B0));               \
-    B1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(B1, B1));               \
-    D0 = _mm_unpackhi_epi64(D0, _mm_unpacklo_epi64(D1, D1));               \
-    D1 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t1, t1));               \
-  } while ((void)0, 0)
+#if defined(__SSSE3__)
+  #define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                            \
+      do {                                                                       \
+          __m128i t0 = _mm_alignr_epi8(B1, B0, 8);                               \
+          __m128i t1 = _mm_alignr_epi8(B0, B1, 8);                               \
+          B0 = t0;                                                               \
+          B1 = t1;                                                               \
+                                                                                \
+          t0 = C0;                                                               \
+          C0 = C1;                                                               \
+          C1 = t0;                                                               \
+                                                                                \
+          t0 = _mm_alignr_epi8(D1, D0, 8);                                       \
+          t1 = _mm_alignr_epi8(D0, D1, 8);                                       \
+          D0 = t1;                                                               \
+          D1 = t0;                                                               \
+      } while ((void)0, 0)
 
+  #define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                          \
+      do {                                                                       \
+          __m128i t0 = _mm_alignr_epi8(B0, B1, 8);                               \
+          __m128i t1 = _mm_alignr_epi8(B1, B0, 8);                               \
+          B0 = t0;                                                               \
+          B1 = t1;                                                               \
+                                                                                \
+          t0 = C0;                                                               \
+          C0 = C1;                                                               \
+          C1 = t0;                                                               \
+                                                                                \
+          t0 = _mm_alignr_epi8(D0, D1, 8);                                       \
+          t1 = _mm_alignr_epi8(D1, D0, 8);                                       \
+          D0 = t1;                                                               \
+          D1 = t0;                                                               \
+      } while ((void)0, 0)
+#else /* SSE2 */
+  #define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                        \
+    do {                                                                     \
+      __m128i t0 = D0;                                                       \
+      __m128i t1 = B0;                                                       \
+      D0 = C0;                                                               \
+      C0 = C1;                                                               \
+      C1 = D0;                                                               \
+      D0 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t0, t0));               \
+      D1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(D1, D1));               \
+      B0 = _mm_unpackhi_epi64(B0, _mm_unpacklo_epi64(B1, B1));               \
+      B1 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(t1, t1));               \
+    } while ((void)0, 0)
+
+  #define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1)                      \
+    do {                                                                     \
+      __m128i t0, t1;                                                        \
+      t0 = C0;                                                               \
+      C0 = C1;                                                               \
+      C1 = t0;                                                               \
+      t0 = B0;                                                               \
+      t1 = D0;                                                               \
+      B0 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(B0, B0));               \
+      B1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(B1, B1));               \
+      D0 = _mm_unpackhi_epi64(D0, _mm_unpacklo_epi64(D1, D1));               \
+      D1 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t1, t1));               \
+    } while ((void)0, 0)
+#endif
 // BLAKE2_ROUND in reference code
 #define P(A0, A1, B0, B1, C0, C1, D0, D1)                                  \
   do {                                                                     \
